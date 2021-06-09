515c44c9a63680169dc81e14efb610e6ccebe567
Nitesh Ramola <niteshramola90@gmail.com>
2021-06-09 16:56:41 +0530

    created productDetailScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Container} from "react-bootstrap";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from "./screens/ProductScreen";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -12,6 +13,7 @@ function App() {
      <Header/>
      <Container>
        <Route path={'/'} component={HomeScreen} exact />
+       <Route path={'/product/:id'} component={ProductScreen} exact />
      </Container>
      <Footer/>
    </Router>
